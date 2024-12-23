eb482b739cdc5c2f568ccb645be4007c6ed7cd76
Daniel Gritzner <dgritzner83@gmail.com>
2024-12-23 13:46:38 +0100

    performance improvement

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,14 +1,14 @@
 use std::collections::{BTreeMap, BTreeSet};
 
-fn get_candidates(computers: &BTreeSet<String>) -> Vec<BTreeSet<String>> {
+fn get_candidates(computers: &BTreeSet<usize>) -> Vec<BTreeSet<usize>> {
     let n = 2_usize.pow(computers.len() as u32);
     let mut candidates = Vec::with_capacity(n);
-    let computers = computers.into_iter().collect::<Vec<_>>();
+    let computers = computers.into_iter().map(|&v| v).collect::<Vec<_>>();
     for i in 0..n {
         let mut candidate = BTreeSet::new();
         for (j, &c) in computers.iter().enumerate() {
             if (i >> j) % 2 == 1 {
-                candidate.insert(c.clone());
+                candidate.insert(c);
             }
         }
         candidates.push(candidate);
@@ -32,20 +32,33 @@ fn main() {
             .split("-")
         )
         .map(|mut s| [
-            s.next().expect("invalid puzzle input").to_owned(), // could probably improve performance by mapping all computer names to unique integers instead
-            s.next().expect("invalid puzzle input").to_owned()
+            s.next().expect("invalid puzzle input"),
+            s.next().expect("invalid puzzle input")
         ])
         .collect::<Vec<_>>();
 
+    let mut computer_ids = BTreeMap::new();
     let mut computers = BTreeMap::new();
     for [c0, c1] in puzzle {
-        computers.entry(c0.clone()).or_insert_with(BTreeSet::new).insert(c1.clone());
+        let n = computer_ids.len();
+        let c0 = *computer_ids.entry(c0).or_insert(n);
+        let n = computer_ids.len();
+        let c1 = *computer_ids.entry(c1).or_insert(n);
+        computers.entry(c0).or_insert_with(BTreeSet::new).insert(c1);
         computers.entry(c1).or_insert_with(BTreeSet::new).insert(c0);
     }
     let mut max_connections = 0;
-    for (c, others) in &mut computers {
-        others.insert(c.clone());
+    for (&c, others) in &mut computers {
+        others.insert(c);
         max_connections = max_connections.max(others.len());
+    }
+    let mut starts_with_t = BTreeSet::new();
+    let mut ids_to_name = vec![""; computer_ids.len()];
+    for (c, id) in computer_ids {
+        ids_to_name[id] = c;
+        if c.starts_with('t') {
+            starts_with_t.insert(id);
+        }
     }
 
     let mut cliques = vec![BTreeSet::new(); max_connections + 1];
@@ -64,7 +77,7 @@ fn main() {
     let mut n = 0;
     'outer: for clique in &cliques[3] {
         for c in clique {
-            if c.starts_with('t') {
+            if starts_with_t.contains(c) {
                 n += 1;
                 continue 'outer;
             }
@@ -76,9 +89,9 @@ fn main() {
         if cliques.is_empty() {
             continue;
         }
-        let mut clique = cliques.into_iter().next().unwrap().into_iter().collect::<Vec<_>>();
+        let mut clique = cliques.into_iter().next().unwrap().into_iter().map(|id| ids_to_name[id]).collect::<Vec<_>>();
         clique.sort();
-        let first = clique[0].clone();
+        let first = clique[0].to_owned();
         println!("[part two] largest clique = {}", clique.into_iter().skip(1).fold(first, |acc, s| format!("{},{}", acc, s)));
         break;
     }
